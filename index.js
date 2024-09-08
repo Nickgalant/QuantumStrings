815f1fbd522b18359794ba6557ca3c6c0c975236
Nicholas Galante Rezende <nicholasgalante@hotmail.co.uk>
2024-09-07 23:11:56 -0300

    deploy vercel

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { NextResponse } from 'next/server';
+import fs from 'fs';
+import path from 'path';
+
+export async function GET() {
+    const filePath = path.join(process.cwd(), 'views', 'index.ejs');
+    const fileContent = fs.readFileSync(filePath, 'utf-8');
+    return NextResponse.json({ content: fileContent });
+}
 
 const app = express();
 const port = 3000;
