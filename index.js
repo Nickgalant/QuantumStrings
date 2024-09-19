0e16e74f1510d8712bfb6c6e0d4ed57e3811bc7e
Nicholas Galante Rezende <nicholasgalante@hotmail.co.uk>
2024-09-19 14:08:40 -0300

    app.post created to create articles

--- index.js
+++ index.js
@@ -8,13 +8,30 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-    res.render("index.ejs");
-})
 
 app.get("/post", (req, res) => {
     res.render("post.ejs");
-})
+});
+
+// Array para armazenar os artigos
+let artigos = [];
+
+app.post("/submit", (req, res) => {
+    const novoArtigo = {
+        titulo: req.body.titulo,
+        texto: req.body.texto
+    };
+    
+    // Adiciona o novo artigo ao array
+    artigos.push(novoArtigo);
+    
+    res.render("index.ejs", { artigos });
+});
+
+app.get("/", (req, res) => {
+    // Renderiza a página com todos os artigos ao carregar
+    res.render("index.ejs", { artigos });
+});
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
